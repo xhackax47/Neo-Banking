ab9cfdbfd298e526c64e7d450ab87a81fb6b0729
xhackax47 <xhackax47@gmail.com>
2024-09-13 19:37:39 +0200

    Ajout construteurs, getters/setters, methodes sur classe Account

--- src/main/java/fr/xhackax47/Neo_Banking/Account.java
+++ src/main/java/fr/xhackax47/Neo_Banking/Account.java
@@ -9,6 +9,53 @@ public class Account {
 	public String Email;
 	public String Téléphone;
 	
+	// Constructeurs
+	public Account(int iD) {
+		super();
+		ID = (int) Math.random();
+	}
 	
-
+	public Account(int iD, String nom, String prénom, String email, String téléphone) {
+		super();
+		ID = (int) Math.random();
+		Nom = nom;
+		Prénom = prénom;
+		Email = email;
+		Téléphone = téléphone;
+	}
+	
+	// Getters/Setters
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public String getNom() {
+		return Nom;
+	}
+	public void setNom(String nom) {
+		Nom = nom;
+	}
+	public String getPrénom() {
+		return Prénom;
+	}
+	public void setPrénom(String prénom) {
+		Prénom = prénom;
+	}
+	public String getEmail() {
+		return Email;
+	}
+	public void setEmail(String email) {
+		Email = email;
+	}
+	public String getTéléphone() {
+		return Téléphone;
+	}
+	public void setTéléphone(String téléphone) {
+		Téléphone = téléphone;
+	}
+	
+	
+	
 }
